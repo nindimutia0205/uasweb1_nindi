f0f1b1b4336c971a55026d08d503616a878a068b
nindimutia0205 <nindimutia1202@gmail.com>
2025-07-14 17:52:12 +0700

    Implementasikan Home Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+    public function index($nama = 'nindi', $pekerjaan = 'desainer')
+    {
+        echo "halo, nama saya $nama cantikkk, saya seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
