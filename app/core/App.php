70f6e936cdb0c1babd08a6377e4ef00861560656
nindimutia0205 <nindimutia1202@gmail.com>
2025-07-14 15:54:49 +0700

    Menambahkan konfigurasi koneksi database (config.php)

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,15 @@
 class App {
     public function __construct()
     {
-        echo 'HAII SAYANG !';
+        $url = $this->parseurl();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if( isset($_GET['url']) ) {
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 }
